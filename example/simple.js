c906ce5c56fa423c918cec035846ef4bf6787d94
juckencia <juzcarvajal@gmail.com>
2015-06-13 11:07:42 -0500

    Ejemplo IC

---
+++ example/simple.js
@@ -0,0 +1,13 @@
+/*
+ * empresariales-ejemplo-ic
+ * https://github.com/juckencia/empresariales-ejemplo-ic
+ *
+ * Copyright (c) 2015 Ju
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var empresarialesEjemploIc = require('../');
+
+console.log(empresarialesEjemploIc()); // "awesome"
